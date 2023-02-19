89f3d43efb0c40681490df0ac705cec807e9ace7
Ashwini <ashwiniparaye9309@gmail.com>
2023-02-19 21:11:26 +0530

    fetching api data

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-fetch("https://type.fit/api/quotes")
+fetch("http://zenquotes.io/api/random")
 .then(data => data.json())
 .then(quotesData => {
     
+    
     for (let i=0; i < quotesData.length; i++){
-        console.log(">>>>>>>", quotesData[i].text)
+        console.log(">>>>>>>", quotesData[i].q)
 
-    const quotesText = quotesData[i].text;
+    const quotesText = quotesData[i].q;
 
-    // console.log("quotes gext==",quotesText)
-    // console.log("quotesData==",quotesData)
+    console.log("quotes text==",quotesText)
+    console.log("quotesData==",quotesData)
     
     const quoteElement = document.getElementById('quoteElement')
     quoteElement.innerHTML = quotesText;
