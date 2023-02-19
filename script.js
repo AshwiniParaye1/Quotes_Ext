806a7c127513b4c650bcdc14f0fc07caa597cc10
Ashwini <ashwiniparaye9309@gmail.com>
2023-02-19 20:28:12 +0530

    fetching api data

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 fetch("https://type.fit/api/quotes")
 .then(data => data.json())
 .then(quotesData => {
-    const quotesData = quotesData.text
+    
+    for (let i=0; i < quotesData.length; i++){
+        console.log(">>>>>>>", quotesData[i].text)
+
+    const quotesText = quotesData[i].text;
+
+    // console.log("quotes gext==",quotesText)
+    // console.log("quotesData==",quotesData)
+    
+    const quoteElement = document.getElementById('quoteElement')
+    quoteElement.innerHTML = quotesText;
+    console.log("quotesText======",quotesText)
+    }
+
 })
