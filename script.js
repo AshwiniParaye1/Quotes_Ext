c060be2cee3a33c946a66b7b5c9d52901b56adba
Ashwini <ashwiniparaye9309@gmail.com>
2023-02-20 14:07:04 +0530

    api error fixed

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-fetch("http://zenquotes.io/api/random")
+// fetch("http://zenquotes.io/api/random")
+// .then(data => data.json())
+// .then(quotesData => {
+    
+    
+//     for (let i=0; i < quotesData.length; i++){
+//         console.log(">>>>>>>", quotesData[i].q)
+       
+//     const quotesText = quotesData[i].q;
+
+//     console.log("quotes text==",quotesText)
+//     console.log("quotesData==",quotesData)
+    
+//     const quoteElement = document.getElementById('quoteElement')
+//     quoteElement.innerHTML = quotesText;
+//     console.log("quotesText======",quotesText)
+//     }
+
+// })
+
+
+fetch("https://api.quotable.io/random")
 .then(data => data.json())
 .then(quotesData => {
     
-    
-    for (let i=0; i < quotesData.length; i++){
-        console.log(">>>>>>>", quotesData[i].q)
-
-    const quotesText = quotesData[i].q;
-
-    console.log("quotes text==",quotesText)
-    console.log("quotesData==",quotesData)
-    
+    const quotesText = quotesData.content;
+    console.log("quotesData====",quotesData)
     const quoteElement = document.getElementById('quoteElement')
     quoteElement.innerHTML = quotesText;
-    console.log("quotesText======",quotesText)
-    }
-
+    console.log("quoteText==", quotesText)
 })
